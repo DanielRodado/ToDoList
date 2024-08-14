763bf646c3beb75923ef46fd73fbb57ab9c9673d
DanielRodado <d4nielrodado@gmail.com>
2024-08-14 18:21:55 -0500

    Métodos e implementados en 'TaskService'

--- src/main/java/com/mindhub/todolist/services/TaskService.java
+++ src/main/java/com/mindhub/todolist/services/TaskService.java
@@ -1,8 +1,13 @@
 package com.mindhub.todolist.services;
 
+import com.mindhub.todolist.dto.TaskDTO;
 import com.mindhub.todolist.models.Task;
 
 public interface TaskService {
+
+    Task getTaskById(Long id);
+
+    TaskDTO getTaskDTOById(Long id);
 
     void saveTask(Task task);
 
--- src/main/java/com/mindhub/todolist/services/implement/TaskServiceImpl.java
+++ src/main/java/com/mindhub/todolist/services/implement/TaskServiceImpl.java
@@ -1,5 +1,7 @@
 package com.mindhub.todolist.services.implement;
 
+import com.mindhub.todolist.dto.TaskDTO;
+import com.mindhub.todolist.exceptions.NotFoundTaskException;
 import com.mindhub.todolist.models.Task;
 import com.mindhub.todolist.repositories.TaskRepository;
 import com.mindhub.todolist.services.TaskService;
@@ -11,6 +13,16 @@ public class TaskServiceImpl implements TaskService {
 
     @Autowired
     private TaskRepository taskRepository;
+
+    @Override
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id).orElseThrow(() -> new NotFoundTaskException("Task not found for the id provided"));
+    }
+
+    @Override
+    public TaskDTO getTaskDTOById(Long id) {
+        return new TaskDTO(getTaskById(id));
+    }
 
     @Override
     public void saveTask(Task task) {
