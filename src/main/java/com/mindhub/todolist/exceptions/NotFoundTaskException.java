71710754a3be703cbf871b39cbd8a7495de3abac
DanielRodado <d4nielrodado@gmail.com>
2024-08-14 18:22:55 -0500

    Excepción 'NotFoundTaskException' creada

---
+++ src/main/java/com/mindhub/todolist/exceptions/NotFoundTaskException.java
@@ -0,0 +1,9 @@
+package com.mindhub.todolist.exceptions;
+
+public class NotFoundTaskException extends RuntimeException {
+
+    public NotFoundTaskException(String message) {
+        super(message);
+    }
+
+}
