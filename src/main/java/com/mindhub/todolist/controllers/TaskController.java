2c4f6a7cc297f288fec105c625608ee073c5741a
DanielRodado <d4nielrodado@gmail.com>
2024-08-14 20:26:26 -0500

    Servlet para crear 'Tasks'

--- src/main/java/com/mindhub/todolist/controllers/TaskController.java
+++ src/main/java/com/mindhub/todolist/controllers/TaskController.java
@@ -1,12 +1,11 @@
 package com.mindhub.todolist.controllers;
 
+import com.mindhub.todolist.dto.TaskApplicationDTO;
+import com.mindhub.todolist.dto.TaskDTO;
 import com.mindhub.todolist.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/task")
@@ -16,8 +15,13 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping("/{taskId}")
-    public ResponseEntity<?> getTaskDTOById(@PathVariable("taskId") Long taskId) {
+    public ResponseEntity<TaskDTO> getTaskDTOById(@PathVariable("taskId") Long taskId) {
         return ResponseEntity.ok(taskService.getTaskDTOById(taskId));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createTask(@RequestBody TaskApplicationDTO taskApp) {
+        return taskService.requestCreateTask(taskApp);
     }
 
 }
