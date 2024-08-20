cfa152f8a0e14b8a87ed93988e32e1c16edbfefa
DanielRodado <d4nielrodado@gmail.com>
2024-08-20 12:51:31 -0500

    Seguridad agregada: Spring Security + JWT

--- src/main/java/com/mindhub/todolist/Util.java
+++ src/main/java/com/mindhub/todolist/Util.java
@@ -5,18 +5,23 @@ import com.mindhub.todolist.models.Task;
 import com.mindhub.todolist.models.UserEntity;
 import com.mindhub.todolist.repositories.TaskRepository;
 import com.mindhub.todolist.repositories.UserEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class Util {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     
     @Bean
     public CommandLineRunner initData(UserEntityRepository userEntityRepository, TaskRepository taskRepository) {
         return args -> {
 
-            UserEntity userEntity = new UserEntity("melba51", "melbamorel@gmail.com", "12345");
+            UserEntity userEntity = new UserEntity("melba51", "melbamorel@gmail.com", passwordEncoder.encode("12345"));
             userEntityRepository.save(userEntity);
 
             Task task = new Task("Organizar el armario","Clasificar y doblar la ropa.", TaskStatus.PENDING);
---
+++ src/main/java/com/mindhub/todolist/configurations/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.mindhub.todolist.configurations;
+
+import com.mindhub.todolist.models.UserEntity;
+import com.mindhub.todolist.repositories.UserEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserEntityRepository userEntityRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userEntityRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return new User(userEntity.getEmail(), userEntity.getPassword(), new ArrayList<>());
+    }
+}
