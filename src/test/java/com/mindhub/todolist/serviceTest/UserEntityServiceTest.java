6917d820cc0caba0cee20856f6a8839f07569c01
DanielRodado <d4nielrodado@gmail.com>
2024-08-22 16:35:36 -0500

    Test para verificar que el servicio de usuario maneja correctamente la lógica para encontrar un usuario por ID

--- src/test/java/com/mindhub/todolist/serviceTest/UserEntityServiceTest.java
+++ src/test/java/com/mindhub/todolist/serviceTest/UserEntityServiceTest.java
@@ -12,7 +12,8 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -27,12 +28,13 @@ public class UserEntityServiceTest {
 
     @Test
     public void testGetUserByUsername() {
-        UserEntity userEntity = new UserEntity("melba", "melba@gmail.com", "123");
-        when(userEntityRepository.findByUsername(anyString())).thenReturn(Optional.of(userEntity));
+        UserEntity mokUuserEntity = new UserEntity("melba", "melba@gmail.com", "1234567890");
+        when(userEntityRepository.findById(anyLong())).thenReturn(Optional.of(mokUuserEntity));
 
-        UserEntity result = userEntityService.findUserEntityByUsername("melba");
+        UserEntity result = userEntityService.findUserEntityById(1L);
 
-        assertEquals("melba", result.getUsername());
+        verify(userEntityRepository).findById(anyLong()); // Verifica que el repositorio haya sido llamado con cualquier ID
+        assertEquals("melba", result.getUsername()); // Verifica que el username sea correcto
     }
 
 }
